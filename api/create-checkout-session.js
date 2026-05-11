270dadee1d04f5bfe901a08c1f4091cc0fb8dc72
tasosthd <8r8sy7x5ys@privaterelay.appleid.com>
2026-05-11 20:26:12 +0300

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -30,16 +30,12 @@ module.exports = async function handler(req, res) {
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-    const origin =
-      req.headers.origin ||
-      "https://taskcoastal.com";
+    const origin = req.headers.origin || "https://taskcoastal.com";
 
     const session = await stripe.checkout.sessions.create({
-      mode: "subscription",
+      payment_method_types: ["card"],
 
-      automatic_payment_methods: {
-        enabled: true
-      },
+      mode: "subscription",
 
       customer_email: email,
 
